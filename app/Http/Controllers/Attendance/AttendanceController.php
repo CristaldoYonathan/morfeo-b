a780134d27b1a8eb9a55debca8673a860e4294d7
Cristaldo Yonathan Ariel <91328244+CristaldoYonathan@users.noreply.github.com>
2024-11-09 20:49:29 -0300

    Update AttendanceController.php

--- app/Http/Controllers/Attendance/AttendanceController.php
+++ app/Http/Controllers/Attendance/AttendanceController.php
@@ -6,12 +6,12 @@ use App\Exceptions\Attendance\EntryAlreadyExistsException;
 use App\Exceptions\Attendance\ExitWithoutEntryException;
 use App\Http\Controllers\Controller;
 use App\Services\AttendanceService;
-use http\Client\Request;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class AttendanceController extends Controller
 {
-    protected $attendanceService;
+    protected AttendanceService $attendanceService;
 
     public function __construct(AttendanceService $attendanceService)
     {
@@ -23,17 +23,19 @@ class AttendanceController extends Controller
         $userId = $request->input('user_id');
         $scanTime = $request->input('scan_time');
 
+//        retornar un json con un ok
+//        return response()->json(['message' => $scanTime, 'tiempo'=>$userId], 200);
         // Determinar si se registra entrada o salida basado en el atributo 'action'
         if ($request->attributes->get('action') === 'entry') {
             try {
-                $attendance = $this->attendanceService->markEntry($userId, $scanTime);
+                $attendance = $this->attendanceService->recordEntry($userId, $scanTime);
                 return response()->json(['message' => 'Entrada registrada.', 'data' => $attendance], 200);
             } catch (EntryAlreadyExistsException $e) {
                 return response()->json(['error' => $e->getMessage()], 400);
             }
         } else {
             try {
-                $attendance = $this->attendanceService->markExit($userId, $scanTime);
+                $attendance = $this->attendanceService->recordExit($userId, $scanTime);
                 return response()->json(['message' => 'Salida registrada.', 'data' => $attendance], 200);
             }catch (ExitWithoutEntryException $e) {
                 return response()->json(['error' => $e->getMessage()], 400);
